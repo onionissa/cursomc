a5ae2e82e5c6854c008dd7a25ee58b8d7b96ef97
Oscar Nunes <oscarnionissa.investments@gmail.com>
2025-06-25 23:44:43 +0200

    Ajustes no endpoint /categorias/{id}

--- src/main/java/com/oscar/cursomc/services/CategoriaService.java
+++ src/main/java/com/oscar/cursomc/services/CategoriaService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.oscar.cursomc.domain.Categoria;
 import com.oscar.cursomc.repositories.CategoriaRepository;
+import com.oscar.cursomc.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -18,8 +19,9 @@ public class CategoriaService {
 	
 	public Categoria buscar(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
-       return obj.orElse(null);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+        		"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
-	
+        		
 
 }
